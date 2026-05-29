81e934d1cb2fa716248f0e23c290e84f4858b3c5
Oaksoft <ghstnetwr0k@gmail.com>
2026-05-29 11:29:09 +0100

    feat: implement fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,15 @@
 import { Stack } from "expo-router";
 import "@/global.css";
+import {useFonts} from "expo-font";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'sans-medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'sans-semibold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+    'sans-extrabold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf')
+  })
+  return <Stack  screenOptions={{headerShown: false}}/>;
 }
